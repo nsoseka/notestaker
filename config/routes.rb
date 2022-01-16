3bd6fbb37da76ff8c2fbb47e02360395e5a6254c
Lukong Isaac <lisaacjunior@yahoo.com>
2022-01-16 01:38:58 +0100

    make notes path root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   resources :notes
+  root "notes#index"
   get 'landing/index'
-  root 'landing#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
